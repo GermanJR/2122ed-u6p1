644a0bf041bef20677fbb9f918104faf400ec1eb
Germán Juan <wambows@gmail.com>
2022-03-19 12:08:21 +0100

    Extretes les constants d'institut

--- src/main/java/ed/German/School.java
+++ src/main/java/ed/German/School.java
@@ -2,14 +2,19 @@ package ed.German;
 
 public class School {
 
+	private static final int ANY = 2022;
+	private static final String CICLE_FORMATIU = "1º DAM-B";
+	private static final String COGNOMS_ALUMNE = "Juan";
+	private static final String NOM_ALUMNE = "Germán";
+
 	public static void main(String[] args) {
 		
 		Persona alumneGerman = new Persona();
 		
-		alumneGerman.nom = "Germán";
-		alumneGerman.cognoms = "Juan";
-		alumneGerman.cicleFormatiu = "1º DAM-B";
-		alumneGerman.any = 2022;
+		alumneGerman.nom = NOM_ALUMNE;
+		alumneGerman.cognoms = COGNOMS_ALUMNE;
+		alumneGerman.cicleFormatiu = CICLE_FORMATIU;
+		alumneGerman.any = ANY;
 		
 		System.out.println(alumneGerman.toString());
 
